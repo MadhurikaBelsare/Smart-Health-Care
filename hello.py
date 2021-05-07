df072f90602cef904a96886054b23a3f3feb1240
MadhurikaBelsare <60507707+MadhurikaBelsare@users.noreply.github.com>
2021-05-07 19:57:16 +0530

    Changes Required

--- hello.py
+++ hello.py
@@ -50,10 +50,10 @@ def Heart():
             result="Congrats our model says he is {}% sure you dont have heart disease".format(int(my_prediction[0][0]*100))
             val="green"
         elif my_prediction[0][1]>0.5:
-            result="sorry to say but our model says there is {} % chance you have a heart disease".format(int(my_prediction[0][1]*100))
+            result="Sorry to say but our model says there is {} % chance you have a heart disease".format(int(my_prediction[0][1]*100))
             val="red"
         else:
-            result="sorry to say our model is not sure about you,its 50-50 condition"
+            result="Sorry to say our model is not sure about you,its 50-50 condition"
             val="black"
     return render_template('Heart.html',prediction = result, color=val, connect=connect)
 
@@ -129,13 +129,13 @@ def diabetes():
         my_prediction = classifier.predict_proba(inputFeature)
         connect=5
         if my_prediction[0][0] > 0.5:
-            result="congrats our model says he is {}% sure you dont have diabetes disease".format(int(my_prediction[0][0]*100))
+            result="Congrats our model says he is {}% sure you dont have diabetes disease".format(int(my_prediction[0][0]*100))
             val="green"
         elif my_prediction[0][1]>0.5:
-            result="sorry to say but our model says there is {} % chance you have a diabetes disease".format(int(my_prediction[0][1]*100))
+            result="Sorry to say but our model says there is {} % chance you have a diabetes disease".format(int(my_prediction[0][1]*100))
             val="red"
         else:
-            result="sorry to say our model is not sure about you,its 50-50 condition"
+            result="Sorry to say our model is not sure about you,its 50-50 condition"
             val="black"
     return render_template('diabetes.html',prediction = result,connect=connect,color=val)
 
@@ -163,13 +163,13 @@ def liver():
         my_prediction = classifier.predict_proba(inputFeature)
         connect=5
         if my_prediction[0][0] > 0.5:
-            result="congrats our model says he is {}% sure you dont have liver disease".format(int(my_prediction[0][0]*100))
+            result="Congrats our model says he is {}% sure you dont have liver disease".format(int(my_prediction[0][0]*100))
             val="green"
         elif my_prediction[0][1]>0.5:
-            result="sorry to say but our model says there is {} % chance you have a liver disease".format(int(my_prediction[0][1]*100))
+            result="Sorry to say but our model says there is {} % chance you have a liver disease".format(int(my_prediction[0][1]*100))
             val="red"
         else:
-            result="sorry to say our model is not sure about you,its 50-50 condition"
+            result="Sorry to say our model is not sure about you,its 50-50 condition"
             val="black"
     return render_template('liver.html',prediction = result,connect=connect,color=val)
 
@@ -210,13 +210,13 @@ def parkirson():
         my_prediction = classifier.predict_proba(inputFeature)
         connect=5
         if my_prediction[0][0] > 0.5:
-            result="congrats our model says he is {}% sure you dont have parkirson disease".format(int(my_prediction[0][0]*100))
+            result="Congrats our model says he is {}% sure you dont have parkirson disease".format(int(my_prediction[0][0]*100))
             val="green"
         elif my_prediction[0][1]>0.5:
-            result="sorry to say but our model says there is {} % chance you have a parkirson disease".format(int(my_prediction[0][1]*100))
+            result="Sorry to say but our model says there is {} % chance you have a parkirson disease".format(int(my_prediction[0][1]*100))
             val="red"
         else:
-            result="sorry to say our model is not sure about you,its 50-50 condition"
+            result="Sorry to say our model is not sure about you,its 50-50 condition"
             val="black"
     return render_template('parkirson.html',prediction = result,connect=connect, color=val)
 
